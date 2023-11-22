19e0b85e4d908abe18ee947369a34ab745008785
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2023-11-22 08:54:17 -0300

    qr page

--- UcaFix/screens/UserScreens/Aula.jsx
+++ UcaFix/screens/UserScreens/Aula.jsx
@@ -4,11 +4,20 @@ import { Keyboard, Platform, StyleSheet, Text, TextInput, TouchableOpacity, Imag
 import styles from '../styles'; 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native-gesture-handler';
+import { SelectList } from 'react-native-dropdown-select-list'
 
 export const Aula = (props) => {
   const [aula, setAula] = React.useState("");
   const [motivo, setMotivo] = React.useState("");
   const [descripcion, setDescripcion] = React.useState("");
+  const [Edificio, setEdificio] = React.useState("");
+  const data2 = [
+    {key:'1', value:'Seleccione', disabled:true},
+    {key:'2', value:'Santa María'},
+    {key:'3', value:'San Alberto Magno'},
+    {key:'4', value:'Santo Tomás Moro'},
+    {key:'5', value:'San José'},
+]
 
     return(
       <ScrollView style= {{backgroundColor:"white"}}>
@@ -24,8 +33,21 @@ export const Aula = (props) => {
             placeholder="Ej: 105"
             placeholderTextColor="#8D8D8D"
             onChangeText={(aula) => setAula(aula)}
-          />
-
+          />    
+      <Text style={[styles.inputTitle,{marginTop:"4%"}]}>Edificio y Piso</Text>
+            <SelectList 
+                search={true} 
+                setSelected={(val) => setEdificio(val)} 
+                boxStyles={{paddingVertical:"5%",borderRadius:10, backgroundColor:"#F9F9F9",borderWidth:0,color:"#8D8D8D"}}
+                searchPlaceholder='Búsqueda'
+                notFoundText='Edificio no encontrado'
+                inputStyles={{borderRadius:6, color:"#8D8D8D"}}
+                dropdownStyles={{borderRadius:6,borderWidth:0,backgroundColor:"#F9F9F9",color:"#8D8D8D"}}
+                dropdownItemStyles={{borderRadius:6,backgroundColor:"#E6E6E6",marginHorizontal:"4%",marginTop:"2%",color:"#8D8D8D"}}
+                dropdownTextStyles={{padding:4,color:"#8D8D8D"}}
+                data={data2} 
+                defaultOption={{ key:'1', value:'Seleccione' }}  
+                save="value"/>
           <Text style={styles.inputTitle}>Motivo</Text>
           <TextInput
             style={styles.input}
@@ -53,7 +75,7 @@ export const Aula = (props) => {
             <Text style={styles.buttonText}>Cargar imagen/es</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.buttonListo} onPress={() => props.navigation.navigate('FinalizarArreglo')}>
+          <TouchableOpacity style={styles.buttonListo} onPress={() => props.navigation.navigate('MainScreen')}>
             <Text style={styles.buttonTextListo}>Listo</Text>
           </TouchableOpacity>
 
